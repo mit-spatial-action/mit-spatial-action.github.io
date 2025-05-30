73afc877183be1c3afab7e6cbef7d0469c3f90e3
Eric Robsky Huntley <ericrobskyhuntley@proton.me>
2025-05-30 14:19:07 -0400

    simplify ur life (paging #10)

--- src/ts/helpers.ts
+++ src/ts/helpers.ts
@@ -1,8 +1,3 @@
-import { type StylesModel } from '@ts/Models'
-
-import * as stylesRaw from 'src/sass/_variables.module.scss';
-const styles:StylesModel = Object(stylesRaw);
-
 function hexToRGB(hex:string) {
     let shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
     hex = hex.replace(shorthandRegex, function(m, r, g, b) {
@@ -14,12 +9,3 @@ function hexToRGB(hex:string) {
         parseInt(result[3], 16)
     ] : null;
 }
-
-function isLight(rgbColor:number[]) {
-    const [r,g,b] = rgbColor;
-    let hsp = Math.sqrt(0.299 * (r * r) + 0.587 * (g * g) + 0.114 * (b * b))
-    return hsp > 127.5 ? true : false;
-}
-const light = isLight(hexToRGB(styles.danger));
-
-export const baseColor = light ? 'has-background-success' : 'has-background-danger';
